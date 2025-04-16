4290733f4be32bdaa7054c6440b2d353657c27b8
songcw8 <ben990158@gmail.com>
2025-04-16 10:50:35 +0900

    setting

--- src/main/java/org/example/bootapi/controller/MainController.java
+++ src/main/java/org/example/bootapi/controller/MainController.java
@@ -1,4 +1,15 @@
 package org.example.bootapi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("message", "깨비드릴조");
+        return "index";
+    }
 }
